4665dea27f6cd3476efc0b970d0d03d5ef89bb6d
Andrei Simionescu <ansimionescu@gmail.com>
2024-12-05 00:19:07 +0000

    Refactoring to use util

--- 2024/03.py
+++ 2024/03.py
@@ -1,9 +1,10 @@
 from re import findall
+from util import *
 
 
 def process(text):
     total = 0
-    for match in findall("mul\(\d{1,3},\d{1,3}\)", text):
+    for match in findall(r"mul\(\d{1,3},\d{1,3}\)", text):
         a, b = map(int, match[4:-1].split(","))
         total += a * b
     return total
@@ -14,14 +15,12 @@ def process_do_dont(text):
     dos = text.split("do()")
     for do in dos:
         sections = do.split("don't()")
-        for match in findall("mul\(\d{1,3},\d{1,3}\)", sections[0]):
+        for match in findall(r"mul\(\d{1,3},\d{1,3}\)", sections[0]):
             a, b = map(int, match[4:-1].split(","))
             total += a * b
     return total
 
 
-with open("03.input") as f:
-    lines = f.readlines()
-    text = "".join(map(lambda l: l.strip(), lines))
-    print(process(text))
-    print(process_do_dont(text))
+text = "".join(read_input())
+print(f"Part 1: {process(text)}")
+print(f"Part 2: {process_do_dont(text)}")
