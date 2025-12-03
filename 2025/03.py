061ac9a1920a95ac66e0362b7062350eedddb210
Andrei Simionescu <andrei.simionescu@nala.money>
2025-12-03 23:00:23 +0000

    Day 3

---
+++ 2025/03.py
@@ -0,0 +1,25 @@
+def solve_line(line, answer_size=2):
+    line = line[::-1]
+
+    answer = list(range(answer_size))
+    for index in range(len(answer) - 1, -1, -1):
+        hi = len(line) if index == answer_size - 1 else answer[index + 1]
+        idx = hi - 1
+        target = max(line[answer[index] : hi])
+        while idx < hi:
+            if line[idx] == target:
+                answer[index] = idx
+                break
+            idx -= 1
+
+    return int("".join(map(lambda x: str(line[x]), answer[::-1])))
+
+
+with open("03.txt") as f:
+    answer1 = answer2 = 0
+    answer_new = 0
+    for line in f.readlines():
+        line = list(map(int, line.strip()))
+        answer1 += solve_line(line)
+        answer2 += solve_line(line, 12)
+    print(answer1, answer2)
